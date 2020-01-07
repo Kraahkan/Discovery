8b0efc9f03fe41ae1364b34d103b962a219ac800
josh <josh@josh.ca>
2020-01-07 11:13:10 -0800

    Added a library, more color changing

--- app/src/main/java/com/example/discovery/MainActivity.java
+++ app/src/main/java/com/example/discovery/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.discovery;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import com.irozon.sneaker.Sneaker;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -41,12 +48,47 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+
+        // Customize colors
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0000ff")));
 
+        Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(Color.parseColor("#0000ff"));
 
 
+        Sneaker.with(this) // Activity, Fragment or ViewGroup
+                .setTitle("Success!!")
+                .setMessage("This is the success message")
+                .sneakSuccess();
 
+
+        // https://github.com/yuyakaido/CardStackView
+        // https://github.com/flschweiger/SwipeStack
+        // https://github.com/UFreedom/FloatingView
+        // https://github.com/XunMengWinter/CircularAnim
+        // https://github.com/JoaquimLey/faboptions
+        // https://github.com/codemybrainsout/ahoy-onboarding
+        // https://github.com/skyfe79/AndroidGradientImageView
+        // https://github.com/Kraahkan?after=Y3Vyc29yOnYyOpK5MjAxNi0xMC0xN1QxNjoxOToyNS0wNzowMM4EMmfr&tab=stars
+        // https://github.com/LiveTyping/CannyViewAnimator
+        // https://github.com/iammert/ScalingLayout
+        // https://github.com/PHELAT/Fun
+        // https://github.com/igalata/Bubble-Picker
+        // https://github.com/jinatonic/confetti
+        // https://github.com/aurelhubert/ahbottomnavigation
+        // https://github.com/ncapdevi/FragNav
+        // https://github.com/Shashank02051997/FancyToast-Android
+        // https://github.com/saket/InboxRecyclerView
 
 
     }
